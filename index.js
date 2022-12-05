ccd929b991e4d3880120074ddd8228798c18b2c7
Hunter O'Neal <hunteroneal54@gmail.com>
2022-12-04 21:24:19 -0600

    added classes and interactions

--- index.js
+++ index.js
@@ -1,4 +1,76 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 const Employee = require('./lib/Employee');
 
-const roster = new Employee();
 
+
+
+
+
+// mainMenu() {
+//     inquirer
+//       .prompt([
+//         {
+//             type: "input",
+//             message: "Welcome Manager! Please enter your name:",
+//             name: "managerName"
+//         },
+//         {
+//             type: "input",
+//             message: "Please enter your employee ID:",
+//             name: "managerID"
+//         },
+//         {
+//             type: "input",
+//             message: "Please enter your email address:",
+//             name: "managerEmail"
+//         },
+//         {
+//             type: "input",
+//             message: "Please enter your office number:",
+//             name: "managerOfficeNumber"
+//         }
+//       ])
+//       .then((answers) => {
+//         inquirer
+//             .prompt([
+//                 {
+//                     type: "list",
+//                     message: "Please choose what you'd like to do:",
+//                     choices: [
+//                         "Add an engineer",
+//                         "Add an intern",
+//                         "Finish building my team"
+//                     ],
+//                     name: "managerChoices"
+//                 }
+//             ])
+//             .then((response) => {
+//                 if(response.managerChoices === "Add an engineer") {
+//                     inquirer
+//                         .prompt([
+//                             {
+//                                 type: "input",
+//                                 message: "What is your new engineer's name?",
+//                                 name: "engineerName"
+//                             },
+//                             {
+//                                 type: "input",
+//                                 message: "What is your new engineer's email?",
+//                                 name: "engineerEmail"
+//                             },
+//                             {
+//                                 type: "input",
+//                                 message: "What is your new engineer's id?",
+//                                 name: "engineerID"
+//                             },
+//                             {
+//                                 type: "input",
+//                                 message: "What is your new engineer's github?",
+//                                 name: "engineerGithub"
+//                             },
+//                         ])
+//                 }
+//             })
+//       }
+// }
